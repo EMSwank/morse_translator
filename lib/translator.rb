b5d45821dd4d2962e1acc84961286bbb14889d5b
Eliot Swank <eliotswank@gmail.com>
2017-12-06 10:55:24 -0700

    Morse Translator--I really struggled with this... :(((

---
+++ lib/translator.rb
@@ -0,0 +1,16 @@
+
+
+morse_dictionary =
+             {"a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".",
+              "f": "..-.", "g": "--.", "h": "....", "i": "..", "j": ".---",
+              "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "--", "q": "--.-",
+              "r": ".-.", "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--",
+              "x": "-..-", "y": "-.--", "z": "--.."}
+
+
+def eng_to_morse(string_to_translate)
+    morse_dictionary.each do |morse_dictionary|
+    end
+end
+
+puts eng_to_morse("hello world")
